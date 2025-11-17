ac011fed8f446801778c305ef3d08af667de6b42
MikeczLilla <mikeczlilla08@gmail.com>
2025-11-17 10:04:51 +0100

    wergt

--- t__bla.sql
+++ t__bla.sql
@@ -6,36 +6,34 @@
 -- Létrehozás ideje: 2025. Nov 17. 09:58
 -- Kiszolgáló verziója: 10.4.28-MariaDB
 -- PHP verzió: 8.2.4
-
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */
+;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */
+;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */
+;
+/*!40101 SET NAMES utf8mb4 */
+;
 --
 -- Adatbázis: `2-es csoport`
 --
 
 -- --------------------------------------------------------
-
 --
 -- Tábla szerkezet ehhez a táblához `tábla`
 --
 
-CREATE TABLE `tábla` (
+CREATE TABLE `tabla` (
   `id` int(11) NOT NULL,
   `felhasznalonev` varchar(50) NOT NULL,
   `email` varchar(50) NOT NULL,
   `jelszo` varchar(255) NOT NULL,
   `szdatum` date NOT NULL,
   `tszam` varchar(25) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_hungarian_ci;
-
+) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_hungarian_ci;
 --
 -- Indexek a kiírt táblákhoz
 --
@@ -43,9 +41,8 @@ CREATE TABLE `tábla` (
 --
 -- A tábla indexei `tábla`
 --
-ALTER TABLE `tábla`
-  ADD PRIMARY KEY (`id`);
-
+ALTER TABLE `tabla`
+ADD PRIMARY KEY (`id`);
 --
 -- A kiírt táblák AUTO_INCREMENT értéke
 --
@@ -53,10 +50,12 @@ ALTER TABLE `tábla`
 --
 -- AUTO_INCREMENT a táblához `tábla`
 --
-ALTER TABLE `tábla`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `tabla`
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */
+;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */
+;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */
+;
